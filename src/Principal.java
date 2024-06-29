39ac3a84434d488e4e24db01181dc0542270002e
Gerardo <everythingissofast@gmail.com>
2024-06-29 16:09:50 -0600

    primer commit uso de una funcion lambda para imprimir una lista de numeros

---
+++ src/Principal.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Principal {
+    public static void main(String[] args) {
+        List<Integer> lista1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        for(Integer i: lista1) {
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+        System.out.println("***********************divison antes de funcion lambda");
+        List<Integer> lista2 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        lista2.stream().filter(i -> i % 2 == 0).forEach(System.out::println);
+    }
+}
